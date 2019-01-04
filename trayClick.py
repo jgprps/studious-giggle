94f246eb5d2ed5ca270369e26c19637d16fb1fcb
jgprps <46361426+jgprps@users.noreply.github.com>
2019-01-03 18:10:47 -0700

    Add files via upload

---
+++ trayClick.py
@@ -0,0 +1,11 @@
+import pyautogui, pyperclip, os, time 
+from PIL import Image
+
+pyautogui.PAUSE = 1
+
+while True:
+   pyautogui.hotkey('win','6')
+   time.sleep(1)
+   pyautogui.hotkey('win','6')
+   time.sleep(500)
+   
